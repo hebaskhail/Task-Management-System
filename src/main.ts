c913ac77d93db7e53f7e7ffa833dc5cb6aa0bffb
Heba Skhail <heba236ah@gmail.com>
2025-04-13 15:50:02 +0200

    Add Interceptor to main file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { BadRequestException, Logger, ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { AuthGuard } from './common/guards/auth.guard';
 import { JwtService } from '@nestjs/jwt';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +22,8 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
 
   app.useGlobalGuards(new AuthGuard(new JwtService(), new Reflector()));
+
+  app.useGlobalInterceptors(new TransformInterceptor( new Logger()));
 
   await app.listen(process.env.PORT ?? 3000);
 }
